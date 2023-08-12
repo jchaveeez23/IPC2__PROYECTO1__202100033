eec7b323da38cf8840bc39581544fdfd58a1d0b0
Josue Chavez <3921213790101@ingenieria.usac.edu.gt>
2023-08-12 16:02:08 -0600

    Se completo la lista enlazada con sus aplicaciones y se realizaron  cargar archivo y procesar

--- main.py
+++ main.py
@@ -19,7 +19,32 @@ class ListaMatriz:
             while actual.next:
                 actual = actual.next
             actual.next = nuevo_nodo
-class senal:
+
+    def encontrar(self,dato):
+        actual = self.cabeza
+        index = 0
+        while actual:
+            if actual.dato.iguales(dato):
+                return index
+            actual = actual.next
+            index +=1
+        return -1
+    
+    def obtener(self,index):
+        actual = self.cabeza
+        for x in range(index):
+            actual = actual.next
+        return actual.dato
+    
+    def tamano(self):
+        actual = self.cabeza
+        contador = 0
+        while actual:
+            contador+=1
+            actual = actual.next
+        return contador
+
+class Senal:
     def __init__(self,nombre,t,A):
         self.nombre = nombre
         self.t = t
@@ -32,6 +57,70 @@ class senal:
 
         def agregar_fila(self,fila):
             self.filas.agregar(fila)
+
+class Senalreducida:
+    def __init__(self,nombre,A):
+        self.nombre = nombre
+        self.A = A
+        self.grupos = ListaMatriz()
+    
+    def agregar_grupo(self,grupo):
+        self.grupos.agregar(grupo)
+
+class Patron:
+    def __init__(self,fila):
+        self.fila = fila
+    
+    def iguales(self,otros):
+        return self.fila == otros.fila
+    
+def cargar_senal(archivo):
+    senales = ListaMatriz()
+    tree = ET.parse(archivo)
+    root = tree.getroot()
+    for senal_elemento in root.findall('./senales/senal'):
+        nombre = senal_elemento.get('nombre')
+        t = int(senal_elemento.get('t'))
+        A = int(senal_elemento.get('A'))
+        senal = Senal(nombre,t,A)
+        matriz = senal.matriz
+        for dato_elemento in senal_elemento.findall('./dato'):
+            fila_m = int(dato_elemento.get('t'))
+            columna_m = int(dato_elemento.get('A'))
+            matriz.agregar_fila(fila_m,columna_m)
+        senales.agregar(senal)
+    return senales
+
+def procesar_senales(senales):
+    senalesreducidas = ListaMatriz()
+    actual = senales.cabeza
+    while actual:
+        senal = actual.dato
+        senalreducida = Senalreducida(senal.nombre,senal.A)
+        patrones = ListaMatriz()
+        for fila_m, fila in enumerate(senal.matriz.filas):
+            patron = Patron(fila)
+            patron_m = patrones.encontrar(patron)
+
+            if patron_m == -1:
+                patrones.agregar(patron)
+                patron_m = patrones.tamano()-1
+
+            patron_dato = patrones.obtener(patron_m)
+            patron_dato['tiempos'].agregar(fila_m)
+
+            for col_m,valor in enumerate(fila):
+                if valor == 1:
+                    dato = {'A':col_m, 'Valor': 1}
+                    patron_dato['datos'].agregar(dato)
+        
+        for patron_dato in patrones:
+            senalreducida.agregar_grupo(patron_dato)
+
+        senalesreducidas.agregar(senalreducida)
+        actual = actual.next
+    return senalesreducidas
+
     
 
 
